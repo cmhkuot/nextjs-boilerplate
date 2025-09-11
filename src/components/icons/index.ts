36de7823a3bf47403d29603ccf96c33a5fc490e3
Son Do <dhson8396@gmail.com>
2025-09-11 21:55:04 +0700

    feat: add Icon component and site configuration constants

--- src/components/icons/index.ts
+++
@@ -1,7 +0,0 @@
-import { Next } from "./next";
-
-const icons = { Next };
-
-export type IconName = keyof typeof icons;
-
-export default icons;
---
+++ src/constants/site.ts
@@ -0,0 +1,19 @@
+export const siteConfig = {
+  name: "Next.js",
+  description: "A React framework for production",
+  url: "https://nextjs.org",
+  ogImage: "https://nextjs.org/og-image.png",
+  links: {
+    twitter: "https://twitter.com/nextjs",
+    github: "https://github.com/vercel/next.js",
+  },
+  logo: {
+    url: "https://nextjs.org/favicon.ico",
+    alt: "Next.js Logo",
+  },
+  favicon: "https://nextjs.org/favicon.ico",
+  themeColor: "#000000",
+  appleTouchIcon: "https://nextjs.org/apple-touch-icon.png",
+  manifest: "https://nextjs.org/manifest.json",
+  keywords: ["Next.js", "React", "Framework", "JavaScript"],
+};
